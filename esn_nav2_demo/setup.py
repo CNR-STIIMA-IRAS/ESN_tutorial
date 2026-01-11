96f3c3f8bc9d424b148b16350ddd4eff273da79e
Roberto Fausti <roberto.fausti@stiima.cnr.it>
2026-01-11 16:50:21 +0100

    add xml directory

--- esn_nav2_demo/setup.py
+++ esn_nav2_demo/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'maps'), glob('maps/*')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'xml'), glob('xml/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
